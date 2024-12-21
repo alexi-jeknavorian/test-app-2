45e35ac6668a3cf2441c0a04d65cdca08d1132b9
alexi-jeknavorian <alexi.jeknavorian@gmail.com>
2024-12-21 14:10:19 -0500

    Initial Commit

---
+++ test-app-2/test_app_2App.swift
@@ -0,0 +1,17 @@
+//
+//  test_app_2App.swift
+//  test-app-2
+//
+//  Created by Alexi Jeknavorian on 2024-12-21.
+//
+
+import SwiftUI
+
+@main
+struct test_app_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test-app-2Tests/test_app_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  test_app_2Tests.swift
+//  test-app-2Tests
+//
+//  Created by Alexi Jeknavorian on 2024-12-21.
+//
+
+import Testing
+@testable import test_app_2
+
+struct test_app_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
